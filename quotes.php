e0cee4410a825aadb5c670014b6702c7c7bfb2a3
xdebbie <deh@live.in>
2020-06-04 00:39:15 +0200

    Add quotes.php

---
+++ quotes.php
@@ -0,0 +1,604 @@
+<?php
+
+$quotes = [
+    [
+        'number' => '#1',
+        'text' => 'The fortune you seek is in another cookie.'
+    ],
+    [
+        'number' => '#2',
+        'text' => 'A closed mouth gathers no feet.'
+    ],
+    [
+        'number' => '#3',
+        'text' => 'A conclusion is simply the place where you got tired of thinking.'
+    ],
+    [
+        'number' => '#4',
+        'text' => 'A cynic is only a frustrated optimist.'
+    ],
+    [
+        'number' => '#5',
+        'text' => 'You will die alone and poorly dressed.'
+    ],
+    [
+        'number' => '#6',
+        'text' => 'Do not mistake temptation for opportunity.'
+    ],
+    [
+        'number' => '#7',
+        'text' => 'He who laughs at himself never runs out of things to laugh at.'
+    ],
+    [
+        'number' => '#8',
+        'text' => 'The road to riches is paved with homework.'
+    ],
+    [
+        'number' => '#9',
+        'text' => 'Help! I am being held prisoner in a fortune cookie factory.'
+    ],
+    [
+        'number' => '#10',
+        'text' => 'It\'s about time I got out of that cookie.'
+    ],
+    [
+        'number' => '#11',
+        'text' => 'Never forget a friend. Especially if he owes you.'
+    ],
+    [
+        'number' => '#12',
+        'text' => 'Only listen to the fortune cookie; disregard all other fortune telling units.'
+    ],
+    [
+        'number' => '#13',
+        'text' => 'That wasn\'t chicken.'
+    ],
+    [
+        'number' => '#14',
+        'text' => 'No snowflake feels responsible in an avalanche.'
+    ],
+    [
+        'number' => '#15',
+        'text' => 'You are not illiterate.'
+    ],
+    [
+        'number' => '#16',
+        'text' => 'Avoid taking unnecessary gambles. Lucky numbers: 12, 15, 23, 28, 37'
+    ],
+    [
+        'number' => '#17',
+        'text' => 'Ask your mom instead of a cookie.'
+    ],
+    [
+        'number' => '#18',
+        'text' => 'Hard work pays off in the future. Laziness pays off now.'
+    ],
+    [
+        'number' => '#19',
+        'text' => 'You think it\'s a secret, but they know.'
+    ],
+    [
+        'number' => '#20',
+        'text' => 'Run.'
+    ],
+    [
+        'number' => '#21',
+        'text' => 'You learn from your mistakes... You will learn a lot today.'
+    ],
+    [
+        'number' => '#22',
+        'text' => 'Never give up. You\'re not a failure if you don\'t give up. <3'
+    ],
+    [
+        'number' => '#23',
+        'text' => 'A very attractive person has a message for you.'
+    ],
+    [
+        'number' => '#24',
+        'text' => 'The greatest risk is not taking one.'
+    ],
+    [
+        'number' => '#25',
+        'text' => 'Jealousy doesn\'t open doors, it closes them!'
+    ],
+    [
+        'number' => '#26',
+        'text' => 'It\'s better to be alone sometimes.'
+    ],
+    [
+        'number' => '#27',
+        'text' => 'Sometimes you just need to lay on the floor.'
+    ],
+    [
+        'number' => '#28',
+        'text' => 'When hungry, order more Chinese food.'
+    ],
+    [
+        'number' => '#29',
+        'text' => 'All your fingers can\'t be of the same length.'
+    ],
+    [
+        'number' => '#30',
+        'text' => 'To be old and wise, you must first be young and stupid.'
+    ],
+    [
+        'number' => '#31',
+        'text' => 'Conquer your fears or they will conquer you.'
+    ],
+    [
+        'number' => '#32',
+        'text' => 'Do not fear what you don\'t know.'
+    ],
+    [
+        'number' => '#33',
+        'text' => 'It never pays to kick a skunk.'
+    ],
+    [
+        'number' => '#34',
+        'text' => 'Lend your money and lose your friend.'
+    ],
+    [
+        'number' => '#35',
+        'text' => 'You will kiss your crush ohhh lalahh!'
+    ],
+    [
+        'number' => '#36',
+        'text' => 'Circumstance does not make the man; it reveals him to himself.'
+    ],
+    [
+        'number' => '#37',
+        'text' => 'The cooler you think you are the dumber you look.'
+    ],
+    [
+        'number' => '#38',
+        'text' => 'Failure is the mother of all success.'
+    ],
+    [
+        'number' => '#39',
+        'text' => 'Don\'t worry, half the people you know are below average. '
+    ],
+    [
+        'number' => '#40',
+        'text' => 'Digital circuits are made from analog parts.'
+    ],
+    [
+        'number' => '#41',
+        'text' => 'You are not a ghost.'
+    ],
+    [
+        'number' => '#42',
+        'text' => 'A clear conscience is usually the sign of a bad memory.'
+    ],
+    [
+        'number' => '#43',
+        'text' => 'See if you can learn anything from the children.'
+    ],
+    [
+        'number' => '#44',
+        'text' => 'What goes around comes around.'
+    ],
+    [
+        'number' => '#45',
+        'text' => 'Cookie says: "You crack me up".'
+    ],
+    [
+        'number' => '#46',
+        'text' => 'Your problem just got bigger. Think, what have you done.'
+    ],
+    [
+        'number' => '#47',
+        'text' => 'Forgive your enemies, but never forget them.'
+    ],
+    [
+        'number' => '#48',
+        'text' => 'I am a fortune. You just broke my little house. Where will I live now?'
+    ],
+    [
+        'number' => '#49',
+        'text' => 'Warning, do not eat your fortune.'
+    ],
+    [
+        'number' => '#50',
+        'text' => 'Oops... Wrong cookie.'
+    ],
+    [
+        'number' => '#51',
+        'text' => 'Dogs have owners, cats have staff.'
+    ],
+    [
+        'number' => '#52',
+        'text' => 'Go ask your mom.'
+    ],
+    [
+        'number' => '#53',
+        'text' => 'Here we go. "Moo Shu Cereal" for breakfast with duck sauce.'
+    ],
+    [
+        'number' => '#54',
+        'text' => 'Good clothes open many doors. Go shopping.'
+    ],
+    [
+        'number' => '#55',
+        'text' => '...?'
+    ],
+    [
+        'number' => '#56',
+        'text' => 'You laugh now, wait until you get home.'
+    ],
+    [
+        'number' => '#57',
+        'text' => 'Oops... Wrong cookie.'
+    ],
+    [
+        'number' => '#58',
+        'text' => 'Your resemblance to a muppet will prevent the world from taking you seriously.'
+    ],
+    [
+        'number' => '#59',
+        'text' => 'There\'s no such thing as an ordinary cat.'
+    ],
+    [
+        'number' => '#60',
+        'text' => 'I cannot help you, for I am just a cookie.'
+    ],
+    [
+        'number' => '#61',
+        'text' => 'Stop eating now. Food poisoning no fun.'
+    ],
+    [
+        'number' => '#62',
+        'text' => 'You are cleverly disguised as responsible adult.'
+    ],
+    [
+        'number' => '#63',
+        'text' => 'Soup was secret family recipe made from toad. Hope you liked!'
+    ],
+    [
+        'number' => '#64',
+        'text' => 'When you look down, all you see is dirt, so keep looking up.'
+    ],
+    [
+        'number' => '#65',
+        'text' => 'All progress occurs because people dare to be different.'
+    ],
+    [
+        'number' => '#66',
+        'text' => 'A pet will always brighten your day.'
+    ],
+    [
+        'number' => '#67',
+        'text' => 'Accept that some days you\'re the pigeon, and some days you\'re the statue.'
+    ],
+    [
+        'number' => '#68',
+        'text' => 'Always keep your words soft and sweet, just in case you have to eat them.'
+    ],
+    [
+        'number' => '#69',
+        'text' => 'Compliments cost nothing yet can give so much.'
+    ],
+    [
+        'number' => '#70',
+        'text' => 'Do not rush through life, pause and enjoy it.'
+    ],
+    [
+        'number' => '#71',
+        'text' => 'Do something unusual tomorrow.'
+    ],
+    [
+        'number' => '#72',
+        'text' => 'If the idea you had three days ago still looks good, do it!'
+    ],
+    [
+        'number' => '#73',
+        'text' => 'If you only speak well of others, you never need to whisper.'
+    ],
+    [
+        'number' => '#74',
+        'text' => 'Labels are for cans not people.'
+    ],
+    [
+        'number' => '#75',
+        'text' => 'Look toward the future, but not so far as to miss today.'
+    ],
+    [
+        'number' => '#76',
+        'text' => 'Measure how far you\'ve come, not where you came from.'
+    ],
+    [
+        'number' => '#77',
+        'text' => 'Many people fail because they quit too soon.'
+    ],
+    [
+        'number' => '#78',
+        'text' => 'Never say never.'
+    ],
+    [
+        'number' => '#79',
+        'text' => 'Others need not lose for you to win.'
+    ],
+    [
+        'number' => '#80',
+        'text' => 'Over deliver. Under promise.'
+    ],
+    [
+        'number' => '#81',
+        'text' => 'Some people must break before becoming whole.'
+    ],
+    [
+        'number' => '#82',
+        'text' => '404: Fortune Cookie Message Not Found!'
+    ],
+    [
+        'number' => '#83',
+        'text' => 'Always finish what you st…'
+    ],
+    [
+        'number' => '#84',
+        'text' => 'An empty stomach is not a good political advisor.'
+    ],
+    [
+        'number' => '#85',
+        'text' => 'Be nice to your kids. They\'ll choose your nursing home.'
+    ],
+    [
+        'number' => '#86',
+        'text' => 'Better poor, young and wise, than rich, old and a fool.'
+    ],
+    [
+        'number' => '#87',
+        'text' => 'Don\'t miss the beautiful colors of the rainbow looking for the pot of gold.'
+    ],
+    [
+        'number' => '#88',
+        'text' => 'Forgive your enemies. It messes with their heads.'
+    ],
+    [
+        'number' => '#89',
+        'text' => 'Go take a rest; you deserve it.'
+    ],
+    [
+        'number' => '#90',
+        'text' => 'Good wine makes a bad head and a good story.'
+    ],
+    [
+        'number' => '#91',
+        'text' => 'Half of all doctors graduated in the bottom half of their class.'
+    ],
+    [
+        'number' => '#92',
+        'text' => 'I don\'t know the key to success, but the key to failure is trying to please everybody.'
+    ],
+    [
+        'number' => '#93',
+        'text' => 'If at first you don\'t succeed, then skydiving definitely isn\'t for you.'
+    ],
+    [
+        'number' => '#94',
+        'text' => 'If everything seems to be going well, you\'ve obviously overlooked something.'
+    ],
+    [
+        'number' => '#95',
+        'text' => 'If you can read this you don\'t need glasses.'
+    ],
+    [
+        'number' => '#96',
+        'text' => 'If you think nobody cares about you, try missing a couple of mortgage payments.'
+    ],
+    [
+        'number' => '#97',
+        'text' => 'Ignorance is a powerful excuse.'
+    ],
+    [
+        'number' => '#98',
+        'text' => 'It is easy to be wise after the event.'
+    ],
+    [
+        'number' => '#99',
+        'text' => 'Just because you think you\'re paranoid. Doesn\'t mean someone isn\'t following you.'
+    ],
+    [
+        'number' => '#100',
+        'text' => 'Laugh in the face of danger, then hide until it goes away.'
+    ],
+    [
+        'number' => '#101',
+        'text' => 'Life is not about how fast you run, or how high you climb, but how well you bounce.'
+    ],
+    [
+        'number' => '#102',
+        'text' => 'Look behind you.'
+    ],
+    [
+        'number' => '#103',
+        'text' => 'Man with one chopstick go hungry.'
+    ],
+    [
+        'number' => '#104',
+        'text' => 'Men love with their eyes; women love with their ears.'
+    ],
+    [
+        'number' => '#105',
+        'text' => 'Money can\'t buy you happiness but it does bring you a more pleasant form of misery.'
+    ],
+    [
+        'number' => '#106',
+        'text' => 'Nappies and politicians need to be changed… often for the same reasons.'
+    ],
+    [
+        'number' => '#107',
+        'text' => 'Never argue with an idiot. They drag you down to their level, then beat you with experience.'
+    ],
+    [
+        'number' => '#108',
+        'text' => 'Never test the depth of water with both feet.'
+    ],
+    [
+        'number' => '#109',
+        'text' => 'Nice to be here? At your age it\'s nice to be anywhere.'
+    ],
+    [
+        'number' => '#110',
+        'text' => 'No one is listening until you make a mistake.'
+    ],
+    [
+        'number' => '#111',
+        'text' => 'No wonder you always go home alone.'
+    ],
+    [
+        'number' => '#112',
+        'text' => 'Now is a good time to buy stocks.'
+    ],
+    [
+        'number' => '#113',
+        'text' => 'People who say they are perfect have already proven themselves wrong.'
+    ],
+    [
+        'number' => '#114',
+        'text' => 'Physical activity will dramatically improve your outlook today.'
+    ],
+    [
+        'number' => '#115',
+        'text' => 'Protective measures will prevent costly disasters.'
+    ],
+    [
+        'number' => '#116',
+        'text' => 'Resting well is as important as working hard.'
+    ],
+    [
+        'number' => '#117',
+        'text' => 'Seven days without laughter makes one weak.'
+    ],
+    [
+        'number' => '#118',
+        'text' => 'Silence may suggest wisdom.'
+    ],
+    [
+        'number' => '#119',
+        'text' => 'Strange that psychics have to ask you for your name.'
+    ],
+    [
+        'number' => '#120',
+        'text' => 'Success is a lousy teacher. It seduces smart people into thinking they can\'t lose.'
+    ],
+    [
+        'number' => '#121',
+        'text' => 'Take a minute to gaze above and beyond your usual view. You may notice some stains on the ceiling.'
+    ],
+    [
+        'number' => '#122',
+        'text' => 'That person with whom you exchanged suggestive glances? No, they didn\'t fancy you.'
+    ],
+    [
+        'number' => '#123',
+        'text' => 'The difference between genius and stupidity is; genius has its limits.'
+    ],
+    [
+        'number' => '#124',
+        'text' => 'The harder you work, the luckier you will get.'
+    ],
+    [
+        'number' => '#125',
+        'text' => 'The impossible is often the untried.'
+    ],
+    [
+        'number' => '#126',
+        'text' => 'The only good is knowledge and the only evil ignorance.'
+    ],
+    [
+        'number' => '#127',
+        'text' => 'The world\'s greatest feats were accomplished by people not smart enough to know they were impossible.'
+    ],
+    [
+        'number' => '#128',
+        'text' => 'There are three types of people the world does not understand: madmen, geniuses and people who mumble.'
+    ],
+    [
+        'number' => '#129',
+        'text' => 'There is always room at the top.'
+    ],
+    [
+        'number' => '#130',
+        'text' => 'Tonight you will be blinded by passion.'
+    ],
+    [
+        'number' => '#131',
+        'text' => 'Use your eloquence where it will do the most good.'
+    ],
+    [
+        'number' => '#132',
+        'text' => 'When in doubt, select one of the classics.'
+    ],
+    [
+        'number' => '#133',
+        'text' => 'Willingness to compromise is often a sign of strength.'
+    ],
+    [
+        'number' => '#134',
+        'text' => 'Wisdom goes not always by years.'
+    ],
+    [
+        'number' => '#135',
+        'text' => 'Women\'s faults are many, men have just two! Everything they say and everything they do!'
+    ],
+    [
+        'number' => '#136',
+        'text' => 'Wow! Bet you aren\'t shy in the changing rooms!'
+    ],
+    [
+        'number' => '#137',
+        'text' => 'You are almost there.'
+    ],
+    [
+        'number' => '#138',
+        'text' => 'You can go anywhere you want if you look serious and carry a clipboard.'
+    ],
+    [
+        'number' => '#139',
+        'text' => 'You don\'t stop laughing because you grow old. You grow old because you stop laughing.'
+    ],
+    [
+        'number' => '#140',
+        'text' => 'Your kindred spirit is vodka.'
+    ],
+    [
+        'number' => '#141',
+        'text' => 'You\'re never a loser until you quit trying.'
+    ],
+    [
+        'number' => '#142',
+        'text' => 'You\'re too good for him.'
+    ],
+    [
+        'number' => '#143',
+        'text' => 'An admirer wants to take you on a South Seas cruise.'
+    ],
+    [
+        'number' => '#144',
+        'text' => 'A girl who is free for the evening can be one of the most expensive things in the world.'
+    ],
+    [
+        'number' => '#145',
+        'text' => 'Life begins at forty!'
+    ],
+    [
+        'number' => '#146',
+        'text' => 'You deserve a hug, right now.'
+    ],
+    [
+        'number' => '#147',
+        'text' => 'A mother understands what a child does not say.'
+    ],
+    [
+        'number' => '#148',
+        'text' => 'Happy Sweet 16, Jackie!!!!'
+    ],
+    [
+        'number' => '#149',
+        'text' => 'Feliz cumpleaños!!!'
+    ],
+    [
+        'number' => '#150',
+        'text' => 'Champagne, cake, and birthday laughter… dieting and aspirins the day after.'
+    ],
+];
